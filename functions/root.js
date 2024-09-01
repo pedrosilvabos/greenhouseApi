3427344c86bc3761c4d466147d38ebc1bc5da15b
Joao Pedro Silva Faria <pedro.silva.bos@gmail.com>
2024-09-01 09:11:06 +0000

    separated serverless

---
+++ functions/root.js
@@ -0,0 +1,14 @@
+exports.handler = async (event, context) => {
+  const clientIp =
+    event.headers['x-forwarded-for'] || event.requestContext.identity.sourceIp;
+  const userAgent = event.headers['user-agent'];
+
+  console.log(
+    `Request Method: ${event.httpMethod}, Request URL: ${event.path}, Client IP: ${clientIp}, User-Agent: ${userAgent}`
+  );
+
+  return {
+    statusCode: 200,
+    body: 'Hello World',
+  };
+};
---
+++ functions/set-relays.js
@@ -0,0 +1,29 @@
+exports.handler = async (event, context) => {
+  const clientIp =
+    event.headers['x-forwarded-for'] || event.requestContext.identity.sourceIp;
+  const userAgent = event.headers['user-agent'];
+
+  console.log(
+    `Request Method: ${event.httpMethod}, Request URL: ${event.path}, Client IP: ${clientIp}, User-Agent: ${userAgent}`
+  );
+
+  const now = new Date();
+  const hour = now.getHours();
+
+  // Determine relay states based on the time of day
+  let lightState = hour >= 6 && hour < 18 ? 'off' : 'on';
+  let waterState = lightState;
+
+  // Prepare control payload
+  const controlPayload = {
+    lights: lightState,
+    fans: 'on',
+    water: waterState,
+  };
+
+  // Respond with control payload
+  return {
+    statusCode: 200,
+    body: JSON.stringify(controlPayload),
+  };
+};
